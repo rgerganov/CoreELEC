824732f320c769478fe6a8798eaabe5ab1ed3c8d
Radoslav Gerganov <rgerganov@gmail.com>
2023-10-27 18:09:21 +0300

    Various tweaks

--- packages/addons/addon-depends/qtbase/package.mk
+++ packages/addons/addon-depends/qtbase/package.mk
@@ -3,92 +3,87 @@
 # Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="qtbase"
-PKG_VERSION="5.14.0"
-PKG_SHA256="4ef921c0f208a1624439801da8b3f4344a3793b660ce1095f2b7f5c4246b9463"
+PKG_VERSION="5.6.2"
+PKG_SHA256="7551f5af1312082c3c8ab52f5e102e3f83aae2dd96dc4a3a9a197f3e58b3214c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://qt-project.org"
-PKG_URL="http://download.qt.io/archive/qt/${PKG_VERSION%.*}/${PKG_VERSION}/submodules/${PKG_NAME}-everywhere-src-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="freetype libjpeg-turbo libpng openssl sqlite zlib"
+PKG_URL="https://download.qt.io/new_archive/qt/5.6/5.6.2/single/qt-everywhere-opensource-src-5.6.2.tar.gz"
+PKG_DEPENDS_TARGET="freetype libjpeg-turbo libpng openssl sqlite zlib icu"
 PKG_LONGDESC="A cross-platform application and UI framework."
 PKG_BUILD_FLAGS="-sysroot"
 
+#-device-option CROSS_COMPILE=/media/Storage/arm/output/meson8b_m201/host/usr/bin/arm-linux-gnueabihf-
+#-device-option 'BR_COMPILER_CFLAGS=-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Os  ' 
+#-device-option 'BR_COMPILER_CXXFLAGS=-D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64  -Os  ' 
+
 PKG_CONFIGURE_OPTS_TARGET="-prefix /usr
-                           -sysroot "${SYSROOT_PREFIX}"
                            -hostprefix "${TOOLCHAIN}"
-                           -device linux-libreelec-g++
-                           -opensource -confirm-license
-                           -release
-                           -optimize-size
-                           -strip
-                           -static
-                           -silent
-                           -force-pkg-config
-                           -make libs
-                           -no-dbus
-                           -no-accessibility
-                           -no-glib
-                           -no-iconv
-                           -no-icu
-                           -qt-pcre
-                           -system-zlib
-                           -no-zstd
-                           -openssl-linked
-                           -no-libproxy
-                           -no-cups
-                           -no-fontconfig
-                           -system-freetype
-                           -no-harfbuzz
-                           -no-opengl
-                           -no-egl
-                           -no-eglfs
-                           -no-gbm
-                           -no-kms
-                           -no-linuxfb
-                           -no-xcb
-                           -no-feature-vnc
-                           -no-feature-sessionmanager
-                           -no-feature-easingcurve
-                           -no-feature-effects
-                           -no-feature-gestures
-                           -no-feature-itemmodel
-                           -no-libudev
-                           -no-evdev
-                           -no-libinput
-                           -no-mtdev
-                           -no-tslib
-                           -no-xkbcommon
-                           -no-gif
-                           -no-ico
-                           -system-libpng
-                           -system-libjpeg
-                           -no-sql-mysql
-                           -system-sqlite"
+						   -headerdir /usr/include/qt5 
+                           -sysroot "${SYSROOT_PREFIX}"
+						   -plugindir /usr/lib/qt/plugins
+						   -examplesdir /usr/lib/qt/examples 
+						   -no-rpath 
+						   -nomake tests
+						   -device linux-libreelec-g++
+						   -optimized-qmake 
+						   -no-cups 
+						   -no-iconv 
+						   -system-zlib 
+						   -system-pcre 
+						   -no-pch 
+						   -shared 
+						   -no-kms 
+						   -release 
+						   -largefile 
+						   -opensource 
+						   -confirm-license 
+						   -no-sql-mysql 
+						   -no-sql-psql 
+						   -system-sqlite 
+						   -gui 
+						   -widgets 
+						   --enable-linuxfb 
+						   -directfb 
+						   -no-xcb 
+						   -opengl es2 
+						   -eglfs 
+						   -no-openssl 
+						   -fontconfig 
+						   -system-libjpeg 
+						   -system-libpng 
+						   -dbus 
+						   -no-tslib 
+						   -glib 
+						   -icu 
+						   -make examples 
+						   -skip qtconnectivity
+						   -no-gstreamer"
 
 configure_target() {
-  QMAKE_CONF_DIR="mkspecs/devices/linux-libreelec-g++"
+  QMAKE_CONF_DIR="qtbase/mkspecs/devices/linux-libreelec-g++"
 
   cd ..
   mkdir -p ${QMAKE_CONF_DIR}
 
   cat >"${QMAKE_CONF_DIR}/qmake.conf" <<EOF
-MAKEFILE_GENERATOR      = UNIX
-CONFIG                 += incremental
-QMAKE_INCREMENTAL_STYLE = sublib
+MAKEFILE_GENERATOR       = UNIX
+CONFIG                  += incremental
+QMAKE_INCREMENTAL_STYLE  = sublib
 include(../../common/linux.conf)
 include(../../common/gcc-base-unix.conf)
 include(../../common/g++-unix.conf)
 load(device_config)
-QMAKE_CC         = ${CC}
-QMAKE_CXX        = ${CXX}
-QMAKE_LINK       = ${CXX}
-QMAKE_LINK_SHLIB = ${CXX}
-QMAKE_AR         = ${AR} cqs
-QMAKE_OBJCOPY    = ${OBJCOPY}
-QMAKE_NM         = ${NM} -P
-QMAKE_STRIP      = ${STRIP}
-QMAKE_CFLAGS     = ${CFLAGS}
-QMAKE_CXXFLAGS   = ${CXXFLAGS}
-QMAKE_LFLAGS     = ${LDFLAGS}
+QMAKE_CC                = $CC
+QMAKE_CXX               = $CXX
+QMAKE_LINK              = $CXX
+QMAKE_LINK_SHLIB        = $CXX
+QMAKE_AR                = $AR cqs
+QMAKE_OBJCOPY           = $OBJCOPY
+QMAKE_NM                = $NM -P
+QMAKE_STRIP             = $STRIP
+QMAKE_CFLAGS = $CFLAGS
+QMAKE_CXXFLAGS = $CXXFLAGS
+QMAKE_LFLAGS = $LDFLAGS
 load(qt_config)
 EOF
 
